e6d30231cb632db0ac98f8e9b93254625fce14df
deebot10 <dee1jhill@gmail.com>
2021-09-08 11:22:58 -0600

    Create test for merchant show

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -1,14 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe 'Merchants API' do
-    
-  it 'get all merchants, a maximum of 20 at a time' do
-    create_list(:merchant, 50)
-    get '/api/v1/merchants'
+  
+  describe 'Merchant Index' do
+    it 'get all merchants, a maximum of 20 at a time' do
+      create_list(:merchant, 50)
+      get '/api/v1/merchants'
+      
+      expect(response).to be_successful
+      
+      merchants = JSON.parse(response.body, symbolize_name: true)
+      expect(merchants['data'].size).to eq(20)
+    end
+  end
 
-    expect(response).to be_successful
+  describe 'Merchant Show' do
+    it 'can find a single merchant' do
+      create_list(:merchant, 50)
+      merchant = Merchant.first
+      get "/api/v1/merchants/#{merchant.id}"
 
-    merchants = JSON.parse(response.body, symbolize_name: true)
-    expect(merchants['data'].size).to eq(20)
+      merch = JSON.parse(response.body, symbolize_name: true) 
+
+      expect(response).to be_successful
+      expect(body[:data][:id]).to eq("#{merchant.id}")
+    end  
   end
 end
